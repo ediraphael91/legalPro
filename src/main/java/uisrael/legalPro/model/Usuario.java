a4884d981f333dfd8f30786720a2caba02c25897
ediraphael91 <ediraphael91@gmail.com>
2025-01-26 21:33:18 -0500

    Modelo usuario

--- src/main/java/uisrael/legalPro/model/Usuario.java
+++ src/main/java/uisrael/legalPro/model/Usuario.java
@@ -1,12 +1,23 @@
 package uisrael.legalPro.model;
 
+import java.io.Serializable;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
 @Entity
-public class Usuario  {
+public class Usuario implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int idUsuario;
 	private String nombre;
 	private String apellido;
