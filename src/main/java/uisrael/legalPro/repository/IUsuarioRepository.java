537036dc930eddef65b711b7730f70be65d51c08
ediraphael91 <ediraphael91@gmail.com>
2025-01-30 23:03:46 -0500

    Actualizacion usuario

--- src/main/java/uisrael/legalPro/repository/IUsuarioRepository.java
+++ src/main/java/uisrael/legalPro/repository/IUsuarioRepository.java
@@ -9,9 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface IUsuarioRepository extends JpaRepository<Usuario,Integer>{
 	
-	@Query("select u from Usuario u where u.id_usuario=?1")
-	public Usuario buscarPorId(Integer id);
 	
-	@Query("select u from Usuario u where u.correo_usu=?1")
-	public Usuario findByCorreo(String correo); 
 }
